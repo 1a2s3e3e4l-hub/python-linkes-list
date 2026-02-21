e51f0b1c72cd92dbd8b07fcffed9ad1e37569fec
aseelsaleh-CE <aseelsaleh091@gmail.com>
2026-02-21 21:38:41 +0200

    feat: implement add method to append elements to the list

--- linked_list.py
+++ linked_list.py
@@ -7,6 +7,16 @@ class linked_list:
         self.head = None
         self.length = 0 
 
+    def append(self, value):
+        node: Node = Node(value)
+        if self.head is None:
+            self.head = node
+        else:
+            node.next = self.head
+            self.head = node
+        self.length += 1
+
+
     
 
 
