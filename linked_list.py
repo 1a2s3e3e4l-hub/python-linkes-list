fe7b14e3d79b238cfa93fdca4f11cd37b81f26e4
aseelsaleh-CE <aseelsaleh091@gmail.com>
2026-02-21 23:37:34 +0200

    feat: implement insert method with index validation

--- linked_list.py
+++ linked_list.py
@@ -7,8 +7,8 @@ class linked_list:
         self.head = None
         self.length = 0 
 
-    def append(self, value):
-        node: Node = Node(value)
+    def append(self, data):
+        node: Node = Node(data)
         if self.head is None:
             self.head = node
         else:
@@ -17,7 +17,28 @@ class linked_list:
         self.length += 1
 
 
+
+    def insert(self, index, data):
+        if index < 0 or index > self.length:
+            raise IndexError("Index out of range")
+
+        new_node = Node(data)
+
+        if index == 0:
+            new_node.next = self.head
+            self.head = new_node
+            self.length += 1 
+            return 
     
+        current = self.head
+        for _ in range(index - 1):
+            current = current.next
+
+        new_node.next = current.next
+        current.next = new_node
+        
+        self.length += 1
+
 
 
    
