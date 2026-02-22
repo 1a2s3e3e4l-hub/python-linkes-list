b74a34f5c4b394636a5b117e2140e2644aead6e1
aseel_saleh <5v6q2ngf6c@privaterelay.appleid.com>
2026-02-22 02:45:26 +0200

    Update linked_list.py

--- linked_list.py
+++ linked_list.py
@@ -2,7 +2,7 @@ class Node :
     def __init__(self ,data):
         self.data = data 
         self.next = None
-class linked_list:
+class LinkedList:
     def __init__(self):
         self.head = None
         self.length = 0 
